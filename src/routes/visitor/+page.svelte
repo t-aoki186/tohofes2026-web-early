048ba8f2fae119bee9c1989e6df02dc516530e5e
taoki <84th.tccaoki@gmail.com>
2026-05-20 21:20:30 +0900

    fix: 来場者の皆様へのページのフォントサイズや行間などを修正

--- src/routes/visitor/+page.svelte
+++ src/routes/visitor/+page.svelte
@@ -49,18 +49,18 @@
 	</div>
 	<section class="container mx-auto mt-15 mb-25">
 		<div class="min-x-full mt-4">
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="fa-solid fa-bag-shopping mr-2"></i>持ち物について
 			</h1>
-			<p class="mb-2 leading-[1.8rem]">
+			<p class="mb-2 leading-[2.2rem] text-lg">
 				校内は土足禁止ですので 、<span class="font-bold text-red-600 underline"
 					>必ず上履きと靴袋をご持参ください</span
 				>。
 			</p>
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="fa-solid fa-circle-info mr-2"></i>お困りの場合
 			</h1>
-			<p class="mb-2 leading-[1.8rem]">
+			<p class="mb-2 leading-[2.2rem] text-lg">
 				迷子を発見した場合、落とし物をした場合、その他お困りの際は、お気軽にお近くの桐朋祭スタッフまでお声がけください。
 				<br />
 				また、万が一不審者や不審物を見かけたら絶対に近づかず、速やかにお近くの桐朋祭スタッフにお知らせください。
@@ -72,23 +72,23 @@
 				alt="桐朋祭スタッフTシャツのデザイン"
 				class="t-shirt-design-img"
 			/>
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="fa-solid fa-money-check-dollar mr-2"></i>お支払いについて
 			</h1>
-			<p class="leading-[1.8rem]">
+			<p class="leading-[2.2rem] text-lg">
 				今年度から、<span class="font-bold text-red-600"
 					>生徒による飲食団体（屋台・喫茶）とジュース売り場は、交通系ICカード決済限定です。</span
 				><br />
 				現金やクレジットカード、PayPayなどの他決済サービスはご利用いただけませんのでご注意ください。
 			</p>
-			<p class="mt-1 leading-[1.8rem]">
+			<p class="mt-1 leading-[2.2rem] text-lg">
 				1階の食堂・パン売り場をご利用の場合は、食堂内の券売機または手売り食券売り場で、あらかじめ「食堂用食券」をお買い求めください。
 				<br />
 				<span class="font-bold text-red-600"
 					>食堂用食券の購入は<span class="underline">現金のみ利用可能</span>です</span
 				>（キャッシュレス決済はご利用いただけません）のでご了承ください。
 			</p>
-			<p class="mb-2 leading-[1.8rem]">
+			<p class="mb-2 leading-[2.2rem] text-lg">
 				<span class="font-bold">食堂・パン売り場をご利用の場合</span>と、<span class="font-bold"
 					>飲食団体（屋台・喫茶）やジュース売り場をご利用の場合</span
 				>
@@ -102,16 +102,16 @@
 			<!--e: お支払いについて-->
 			<!---->
 			<!--s: 災害が発生した場合-->
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="fa-solid fa-circle-exclamation mr-2"></i>災害が発生した場合
 			</h1>
-			<p class="leading-[1.8rem]">
+			<p class="leading-[2.2rem] text-lg">
 				<span class="font-bold">地震が発生した場合･･･</span>
 				<br />
 				<span class="font-bold text-red-600">まずはその場で身の安全を確保してください。</span
 				>本校舎は耐震構造になっており、安全です。指示があるまでは校舎内に留まるようお願いします。
 			</p>
-			<p class="mb-2 leading-[1.8rem]">
+			<p class="mb-2 leading-[2.2rem] text-lg">
 				<span class="font-bold">火災が発生した場合･･･</span>
 				<br />
 				<span class="font-bold text-red-600">火元には絶対に近づかず</span
@@ -122,10 +122,10 @@
 			<!--e: 災害が発生した場合-->
 			<!---->
 			<!--s: 校内でのお食事について-->
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="fa-solid fa-utensils mr-2"></i>校内でのお食事について
 			</h1>
-			<p class="mb-2 leading-[1.8rem]">
+			<p class="mb-2 leading-[2.2rem] text-lg">
 				お食事は、指定された場所（食堂、屋台スペースのテント、特別教室5・6、中学棟2階休憩所）以外ではご遠慮ください。
 				<br />
 				また、校内で出たゴミは、指定の場所に分別をして処分していただきますようお願いいたします。
@@ -135,10 +135,10 @@
 			<!--e: 校内でのお食事について-->
 			<!---->
 			<!--s: 小さなお子様をお連れの方へ-->
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="tf26-icon-material icon-baby-bottle mr-2"></i>小さなお子様をお連れの方へ
 			</h1>
-			<p class="mb-2 leading-[1.8rem]">
+			<p class="mb-2 leading-[2.2rem] text-lg">
 				小さなお子様をお連れの方で、授乳が必要な方は、総合案内所(1階)の桐朋祭スタッフまでお声がけください。
 				<br />
 				なお、1階のホール付近と食堂横中庭付近に、おむつ交換台つきの多目的トイレがございます。
@@ -154,10 +154,10 @@
 			<!--e: 小さなお子様をお連れの方へ-->
 			<!---->
 			<!--s: 体調がすぐれない場合-->
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="tf26-icon-material icon-poor-health mr-2"></i>体調がすぐれない場合
 			</h1>
-			<p class="mb-2 leading-[1.8rem]">
+			<p class="mb-2 leading-[2.2rem] text-lg">
 				体調がすぐれない場合、ご気分の悪い場合は、決して無理をなさらず、お近くの桐朋祭スタッフまでお声がけください。
 				<br />
 				保健室へお連れいたします。
@@ -165,10 +165,10 @@
 			<!--e: 体調がすぐれない場合-->
 			<!---->
 			<!--s: その他-->
-			<h1 class="mt-4 text-2xl font-bold text-(--main-text-color)">
+			<h1 class="mt-4 text-[1.7rem] font-bold text-(--main-text-color)">
 				<i class="fa-solid fa-handshake mr-2"></i>その他
 			</h1>
-			<ul class="ml-4 leading-[1.8rem]">
+			<ul class="ml-4 leading-[2.2rem] text-lg">
 				<li class="list-disc">
 					校内での飲酒・喫煙は固くお断りします。<br
 					/>飲酒・喫煙が発覚した場合、ご退場いただく場合がございます。
